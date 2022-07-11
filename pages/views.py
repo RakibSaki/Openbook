ff64efcd73cce6023ef338f1a37ec3e7367e2f7f
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-07-11 21:34:00 +0600

    Fix login message and Add logout

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from audioop import reverse
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
@@ -12,6 +12,7 @@ def home(request):
     return render(request, 'pages/home.html')
 
 def login_view(request):
+    message = ''
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -20,10 +21,14 @@ def login_view(request):
             login(request, user)
             return redirect(reverse_lazy('home'))
         else:
-            return redirect(reverse_lazy('login') + '?message=Invalid%20Credentials')
-    return render(request, 'registration/login.html')
+            message = 'Invalid Credentials'
+    return render(request, 'registration/login.html', {'message':message})
 
 class SignUp(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse_lazy('login'))
